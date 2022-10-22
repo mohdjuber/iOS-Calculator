5118dcbe333b84a545112bfb2b39e34d0ad36379
Mohd Juber Ali <62383865+mohdjuber@users.noreply.github.com>
2022-10-22 11:34:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ export default function App() {
   return (
     <div className="App">
       <Calculator />
-      <h3>
-        iOS Calculator Clone Made By <span>Zubair Rao!</span>
-      </h3>
     </div>
   );
 }
